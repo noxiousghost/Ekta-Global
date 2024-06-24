b5ed9d214ac35cd6f27ced3962593642df550371
saurav tiwari <sauravtiwari145@gmail.com>
2024-06-24 22:15:02 +0545

    footer

---
+++ Client/src/Components/Footer.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const Footer = () => {
+  return (
+    <footer className='bg-black'>
+    <div className='grid md:grid-cols-4 grid-cols-2 md:gap-16 gap-8 py-14 shadow-lg pl-6 text-gray-400  '>
+        <div className='flex flex-col gap-4'>
+            <img src="/Images/logo.png" width={200} alt="logo" />
+            {/* <p className='text-center px-4'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus, ducimus pariatur. Molestias atque ducimus doloribus.</p> */}
+        </div>
+
+        <div className='flex flex-col gap-2'>
+            <h1 className='md:mb-2 text-xl font-semibold text-white'>Company</h1>
+            <Link to='/'>Solutions For Marketting</Link>
+            <Link to='/' >Solutions For Sales</Link>
+            <Link to='/'>Pricing Plans</Link>
+            <Link to='/'>Contact</Link>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+            <h1  className='md:mb-2 text-xl font-semibold text-white'>Integrations</h1>
+            <Link to='/' >Use Case</Link>
+            <Link to='/' >Integrations</Link>
+            <Link to='/' >Examples</Link>
+            <Link to='/' >API Docs</Link>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+            <h1  className='md:mb-2 text-xl font-semibold text-white'>Resources</h1>
+            <Link to='/'>About US</Link>
+            <Link to='/' >Blogs</Link>
+            <Link to='/' >Docs</Link>
+        </div>
+        
+    </div>
+
+    <hr/>
+
+    <div className='flex justify-between px-6 py-2 items-center'>
+        <div className='flex gap-2 items-center'>
+        <i className="ri-copyright-line text-white w-7 text-2xl"></i>
+        <p className='text-white'>2024 Ekta-Global Pvt. Ltd</p>
+        </div>
+        <div className='flex gap-4'>
+            <Link to='https://www.facebook.com/'>
+            <i className="ri-facebook-fill text-white text-2xl" ></i>
+            </Link>
+
+            <Link to='https://www.instagram.com/'>
+            <i className="ri-instagram-line text-white text-2xl"></i>
+            </Link>
+
+            <Link to='https://x.com/'>
+            <i className="ri-twitter-line text-white text-2xl"></i>
+            </Link>
+        
+        </div>  
+    </div>
+
+    </footer>
+  )
+}
+
+export default Footer
--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './Nav'
+import Footer from './Footer'
 
 const Home = () => {
   return (
     <div>
         <Nav/>
+        <Footer/>
     </div>
   )
 }
