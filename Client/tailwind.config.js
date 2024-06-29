641c76a4334f0fa09e911a34748efca22563163a
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-06-29 23:19:22 +0545

    Custom Styling

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dmSans: ["DM+Sans", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+      keyframes: {
+        radiusAnimation: {
+          "0%": { borderRadius: "60% 40% 30% 70%/60% 30% 70% 40%" },
+          "50%": { borderRadius: "30% 60% 70% 40%/50% 60% 30% 60%" },
+          "100%": { borderRadius: "60% 40% 30% 70%/60% 30% 70% 40%" },
+        },
+      },
+      animation: {
+        "radius-animation": "radiusAnimation 5s ease-in-out infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
