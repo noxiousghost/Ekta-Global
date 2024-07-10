9e93d52b45a96e1365cb4d334d37135a4b4976da
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-07-10 21:08:51 +0545

    Add Sponsor Section

--- Client/src/single-pages/Home.jsx
+++ Client/src/single-pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import heroImg from "../../public/Images/hero2.jpg";
 import { Link } from "react-router-dom";
-import { Button } from "antd";
+import Sponsor from "../Components/Sponsor";
 
 const Home = () => {
   return (
     <main className="page_content">
-      <div className="bg-gradient-to-b from-transparent via-[rgba(141,239,255,0.5)] to-[rgba(255,175,251,0.7)] py-16 md:py-20 lg:py-24">
+      <section
+        className="pb-24 pt-12 md:pb-60 md:pt-28 lg:pb-80"
+        style={{
+          background:
+            "linear-gradient(0deg, transparent 5%, rgba(141, 239, 255, 0.5), rgba(255, 175, 251, 0.7))",
+        }}
+      >
         <div className="container max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-wrap md:flex-nowrap items-center gap-6">
             {/* Image on top for mobile, right for desktop */}
@@ -23,7 +29,10 @@ const Home = () => {
             {/* Text Content below image for mobile, left for desktop */}
             <div className="md:flex-1 p-4 order-2 md:order-1">
               <div className="banner_content">
-                <h1 className="font-dmSans font-bold text-4xl md:text-5xl lg:text-6xl text-black leading-tight">
+                <h1
+                  className="font-dmSans font-bold text-4xl md:text-5xl lg:text-6xl text-black"
+                  style={{ lineHeight: "1.25" }}
+                >
                   <span className="text-violet-600">Study in</span> South Korea
                   From Nepal in a Professional way!
                 </h1>
@@ -32,7 +41,7 @@ const Home = () => {
                   transformation in digital technologies.
                 </p>
                 <div className="flex items-center">
-                  <button className="rounded px-6 py-2 text-white bg-violet-500 hover:bg-violet-600 transition-colors">
+                  <button className="rounded px-7 py-4 text-white bg-violet-500 hover:bg-violet-600 hover:shadow-xl hover:shadow-black transition-colors">
                     Go for Educational Learning!
                   </button>
                 </div>
@@ -40,7 +49,18 @@ const Home = () => {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="sponsor_section">
+        <div className="container max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="mb-16 text-center">
+            <h2 className="text-4xl font-bold font-dmSans">
+              We are powered by industry.
+            </h2>
+          </div>
+          <Sponsor />
+        </div>
+      </section>
+      <section className="pt-32"></section>
     </main>
   );
 };
