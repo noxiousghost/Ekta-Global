b2d53bb3aa16737bd4c2cfee5d3bd3031b396667
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-07-13 09:00:15 +0545

    add and implement react creative cursor package

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
-import "./cursor.css";
 
 import Home from "./single-pages/Home";
 import Blog from "./single-pages/Blog";
@@ -10,20 +9,13 @@ import StudentTestimonials from "./single-pages/StudentTestimonials";
 import Notfound from "./Components/Notfound";
 import Nav from "./Components/Nav";
 import Footer from "./Components/Footer";
+import { Cursor } from "react-creative-cursor";
+import "react-creative-cursor/dist/styles.css";
 
 const App = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "/cursor/cursor.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
   return (
     <BrowserRouter>
+      <Cursor isGelly={true} cursorSize={16} cursorBackgrounColor="#3a10e5" />
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
